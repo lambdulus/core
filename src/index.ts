a56435b1f5f232b4ef3197112c596e672efbf0d0
jalivert <184747279+jalivert@users.noreply.github.com>
2018-11-12 19:33:24 +0100

    add some use cases

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import tokenize, { Token } from './lexer'
+
+const inputs : Array<string> = [
+  '(λ ab . + ab)',  // singleLetterVars : true
+  '(λ a b . + a b)',
+  '+ (* 4 5) D',
+  'A (B +) C',
+  '+ (+ A B) C',
+  '(+ A B)',
+  '+ 555 6',
+]
+
+const tokens : Array<Token> = tokenize(inputs[0], {
+  singleLetterVars : false,
+  lambdaLetters : [ 'λ' ],
+  macros : [ ],
+})
+
+tokens.forEach(token => console.log(token))
