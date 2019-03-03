9f2f19ca540160ffdd60be106b19ae60e3be892c
Taskkill <johny95wil@gmail.com>
2019-03-03 18:42:58 +0100

    recompile import changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
 export { Token, tokenize, default as Lexer } from './lexer';
-export { AST, ReductionResult, parse, default as Parser } from './parser/parser';
+export { parse, AST, NextReduction, NextAlpha, NextBeta, NextExpansion, NextNone, Child, ReductionResult, default as Parser } from './parser/parser';
