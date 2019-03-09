81f8b171f07dd17387535d52bec9ad822c4b278c
Taskkill <johny95wil@gmail.com>
2019-03-09 18:14:55 +0100

    implements every rename as single NextAlpha

--- src/visitors/alphaconvertor.ts
+++ src/visitors/alphaconvertor.ts
@@ -1,4 +1,4 @@
-import { ASTVisitor, NextAlpha, Child } from ".";
+import { ASTVisitor, NextAlpha, Child, SingleAlpha } from ".";
 import { Application } from "../parser/ast/application";
 import { Lambda } from "../parser/ast/lambda";
 import { ChurchNumber } from "../parser/ast/churchnumber";
@@ -8,22 +8,43 @@ import { AST } from "../parser";
 import { Token } from "../lexer";
 
 export class AlphaConvertor implements ASTVisitor {
-  public readonly tree : AST
-  private readonly child : Child // not really needed
-  private readonly oldName : string
-  private readonly newName : string
+  public readonly conversions : Array<SingleAlpha>
+  // public readonly tree : Application
+  // private readonly child : Child // not really needed
+  // private readonly toRename : AlphaMap
+  // private readonly oldName : string
+  // private readonly newName : string
 
   // Need to do this Nonsense Dance
   private converted : AST | null = null
-  
-  constructor ({ tree, child, oldName, newName } : NextAlpha) {
-    this.tree = tree
-    this.child = child
-    this.oldName = oldName
-    this.newName = newName
 
-    tree[<Child> child].visit(this)
-    tree[<Child> child] = <AST> this.converted // part of the Nonse Dance
+  private oldName : string = ''
+  private newName : string = ''
+  
+  // TODO: refactor
+  constructor ({ conversions } : NextAlpha) {
+    this.conversions = conversions
+    // this.tree = tree
+    // this.child = child
+    // this.toRename = toRename
+    // this.oldName = oldName
+    // this.newName = newName
+
+    for (const { tree, oldName, newName } of this.conversions) {
+      this.oldName = oldName
+      this.newName = newName
+
+
+
+      tree.argument.visit(this)
+      tree.argument = <Variable> this.converted
+
+      tree.body.visit(this)
+      tree.body = <AST> this.converted
+    }
+
+    // tree[<Child> child].visit(this)
+    // tree[<Child> child] = <AST> this.converted // part of the Nonse Dance
   }
 
   onApplication(application : Application) : void {
@@ -39,18 +60,35 @@ export class AlphaConvertor implements ASTVisitor {
   }
 
   onLambda(lambda : Lambda) : void {
-    lambda.argument.visit(this)
+    // let shadowed : SingleAlpha | null = null
 
-    const left : Variable = <Variable> this.converted
+    // if (this.toRename.has(lambda.argument.name())) {
+      // shadowed = <SingleAlpha> this.toRename.get(lambda.argument.name())
+      // this.toRename.delete(lambda.argument.name())
+    // }
 
-    lambda.body.visit(this)
+    // lambda.argument.visit(this)
 
-    const right : AST = <AST> this.converted
+    // const left : Variable = <Variable> this.converted
 
-    lambda.argument = left
-    lambda.body = right
+    if (lambda.argument.name() !== this.oldName) {
+      lambda.body.visit(this)
+      const right : AST = <AST> this.converted
+      lambda.body = right
+      this.converted = lambda
+    }
+    else {
+      this.converted = lambda
+    }
 
-    this.converted = lambda
+
+
+    // if (shadowed !== null) {
+    //   this.toRename.set(lambda.argument.name(), shadowed)
+    // }
+
+    // lambda.argument = left
+
   }
 
   onChurchNumber(churchNumber : ChurchNumber) : void {
@@ -62,6 +100,8 @@ export class AlphaConvertor implements ASTVisitor {
   }
 
   onVariable(variable : Variable) : void {
+    // if (this.toRename.has(variable.name())) {
+    //   const { newName } : SingleAlpha = <SingleAlpha> this.toRename.get(variable.name())
     if (variable.name() === this.oldName) {
       const token : Token = new Token(variable.token.type, this.newName, variable.token.position)
     
