398930a5562ea63ab0e9fb866e87cefe410334f6
Taskkill <johny95wil@gmail.com>
2019-03-15 00:18:17 +0100

    clean lexer structure

---
+++ src/lexer/token.ts
@@ -0,0 +1,20 @@
+import { PositionRecord } from "./counter";
+
+
+export enum TokenType {
+  Lambda = 'lambda',
+  Dot = 'dot',
+  Identifier = 'identifier',
+  Number = 'number',
+  Operator = 'operator',
+  LeftParen = 'left paren',
+  RightParen = 'right paren',
+}
+
+export class Token {
+  constructor (
+    public readonly type : TokenType,
+    public readonly value : string | number,
+    public readonly position : PositionRecord,
+  ) {}
+}
