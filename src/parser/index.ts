1287a9e823d710431242a079f872512a91cee3ff
Taskkill <johny95wil@gmail.com>
2019-03-24 16:51:22 +0100

    refactor macros

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -13,8 +13,35 @@ export interface MacroTable {
   [ name : string ] : MacroDef
 }
 
-export interface UserMacroTable {
+export interface MacroMap {
   [ name : string ] : string
+}
+
+export const builtinMacros : MacroMap = {
+  'Y' : '(λ f . (λ x . f (x x)) (λ x . f (x x)))',
+  'ZERO' : '(λ n . n (λ x . (λ t f . f)) (λ t f . t))',
+  'PRED' : '(λ x s z . x (λ f g . g (f s)) (λ g . z) (λ u . u))',
+  'SUC' : '(λ n s z . s (n s z))',
+  'AND' : '(λ x y . x y x)',
+  'OR' : '(λ x y . x x y)',
+  'NOT' : '(λ p . p F T)',
+  'T' : '(λ t f . t)',
+  'F' : '(λ t f . f)',
+  '+' : '(λ x y s z . x s (y s z))',
+  '-' : '(λ m n . (n PRED) m)',
+  '*' : '(λ x y z . x (y z))',
+  '/' : '(λ n . Y (λ c n m f x . (λ d . ZERO d (0 f x) (f (c d m f x))) (- n m)) (SUC n))',
+  '^' : '(λ x y . x y)',
+  'DELTA' : '(λ m n . + (- m n) (- n m))',
+  '=' : '(λ m n . ZERO (DELTA m n))',
+  '>' : '(λ m n . NOT (ZERO (- m n)))',
+  '<' : '(λ m n . > n m )',
+  '>=' : '(λ m n . ZERO (- n m))',
+  '<=' : '(λ m n . ZERO (- m n))',
+  'IF' : '(λ p t e . p t e)',
+  'PAIR' : '(λ f s . (λ g . g f s))',
+  'FIRST' : '(λ p . p (λ f s . f))',
+  'SECOND' : '(λ p . p (λ f s . s))',
 }
 
 function toAst (definition : string, macroTable : MacroTable) : AST {
@@ -25,43 +52,14 @@ function toAst (definition : string, macroTable : MacroTable) : AST {
 }
 
 // TODO: refactor macroTable for usage with user defined macro definitions
-export function parse (tokens : Array<Token>, userMacros : UserMacroTable) : AST {
+export function parse (tokens : Array<Token>, userMacros : MacroMap) : AST {
   const macroTable : MacroTable = {}
 
-  macroTable['Y'] = new MacroDef(toAst(`(λ f . (λ x . f (x x)) (λ x . f (x x)))`, macroTable)),
+  for (const [ name, definition ] of Object.entries(builtinMacros)) {
+    macroTable[name] = new MacroDef(toAst(definition, macroTable))
+  }
 
-  macroTable['ZERO'] = new MacroDef(toAst(`(λ n . n (λ x . (λ t f . f)) (λ t f . t))`, macroTable)),
-  
-  macroTable['PRED'] = new MacroDef(toAst(`(λ x s z . x (λ f g . g (f s)) (λ g . z) (λ u . u))`, macroTable))
-  macroTable['SUC'] = new MacroDef(toAst(`(λ n s z . s (n s z))`, macroTable))
-    
-  macroTable['AND'] = new MacroDef(toAst(`(λ x y . x y x)`, macroTable))
-  macroTable['OR'] = new MacroDef(toAst(`(λ x y . x x y)`, macroTable))
-  macroTable['NOT'] = new MacroDef(toAst(`(λ p . p F T)`, macroTable))
-  
-  macroTable['T'] = new MacroDef(toAst(`(λ t f . t)`, macroTable)),
-  macroTable['F'] = new MacroDef(toAst(`(λ t f . f)`, macroTable)),
-  
-  macroTable['+'] = new MacroDef(toAst(`(λ x y s z . x s (y s z))`, macroTable)),
-  macroTable['-'] = new MacroDef(toAst(`(λ m n . (n PRED) m)`, macroTable))
-  macroTable['*'] = new MacroDef(toAst(`(λ x y z . x (y z))`, macroTable)),
-  macroTable['/'] = new MacroDef(toAst(`(λ n . Y (λ c n m f x . (λ d . ZERO d (0 f x) (f (c d m f x))) (- n m)) (SUC n))`, macroTable))
-  macroTable['^'] = new MacroDef(toAst(`(λ x y . x y)`, macroTable))
-  
-  macroTable['DELTA'] = new MacroDef(toAst(`(λ m n . + (- m n) (- n m))`, macroTable))
-  
-  macroTable['='] = new MacroDef(toAst(`(λ m n . ZERO (DELTA m n))`, macroTable))
-  macroTable['>'] = new MacroDef(toAst(`(λ m n . NOT (ZERO (- m n)))`, macroTable))
-  macroTable['<'] = new MacroDef(toAst(`(λ m n . > n m )`, macroTable))
-  macroTable['>='] = new MacroDef(toAst(`(λ m n . ZERO (- n m))`, macroTable))
-  macroTable['<='] = new MacroDef(toAst(`(λ m n . ZERO (- m n))`, macroTable))
-
-  macroTable['IF'] = new MacroDef(toAst(`(λ p t e . p t e)`, macroTable))
-  macroTable['PAIR'] = new MacroDef(toAst(`(λ f s . (λ g . g f s))`, macroTable))
-  macroTable['FIRST'] = new MacroDef(toAst(`(λ p . p (λ f s . f))`, macroTable))
-  macroTable['PAIR'] = new MacroDef(toAst(`(λ p . p (λ f s . s))`, macroTable))
   // TODO: chtel bych LIST, CONS, APPEND, GET NTH ITEM, MAP, ...
-
 
   // QUICK MACROS - non recursively defined
   // macroTable['NOT'] = new MacroDef(toAst(`(λ p . p (λ t f . f) (λ t f . t))`, macroTable))
@@ -75,6 +73,10 @@ export function parse (tokens : Array<Token>, userMacros : UserMacroTable) : AST
   // macroTable['<='] = new MacroDef(toAst(`(λ m n . (λ n . n (λ x . (λ t f . f)) (λ t f . t)) ((λ m n . (n (λ x s z . x (λ f g . g (f s)) (λ g . z) (λ u . u))) m) m n))`, macroTable))
 
   for (const [ name, definition ] of Object.entries(userMacros)) {
+    if (name in builtinMacros) {
+      // TODO: maybe dont throw? better throw, just to be sure :D
+      throw new Error('Cannot redefine built-in Macro ' + name)
+    }
     macroTable[name] = new MacroDef(toAst(definition, macroTable))
   }
 
@@ -82,33 +84,6 @@ export function parse (tokens : Array<Token>, userMacros : UserMacroTable) : AST
 
   return parser.parse(null)
 }
-
-export const builtinMacros : Array<Array<string>> = [
-  ['Y', '(λ f . (λ x . f (x x)) (λ x . f (x x)))'],
-  ['ZERO', '(λ n . n (λ x . (λ t f . f)) (λ t f . t))'],
-  ['PRED', '(λ x s z . x (λ f g . g (f s)) (λ g . z) (λ u . u))'],
-  ['SUC', '(λ n s z . s (n s z))'],
-  ['AND', '(λ x y . x y x)'],
-  ['OR', '(λ x y . x x y)'],
-  ['NOT', '(λ p . p F T)'],
-  ['T', '(λ t f . t)'],
-  ['F', '(λ t f . f)'],
-  ['+', '(λ x y s z . x s (y s z))'],
-  ['-', '(λ m n . (n PRED) m)'],
-  ['*', '(λ x y z . x (y z))'],
-  ['/', '(λ n . Y (λ c n m f x . (λ d . ZERO d (0 f x) (f (c d m f x))) (- n m)) (SUC n))'],
-  ['^', '(λ x y . x y)'],
-  ['DELTA', '(λ m n . + (- m n) (- n m))'],
-  ['=', '(λ m n . ZERO (DELTA m n))'],
-  ['>', '(λ m n . NOT (ZERO (- m n)))'],
-  ['<', '(λ m n . > n m )'],
-  ['>=', '(λ m n . ZERO (- n m))'],
-  ['<=', '(λ m n . ZERO (- m n))'],
-  ['IF', '(λ p t e . p t e)'],
-  ['PAIR', '(λ f s . (λ g . g f s))'],
-  ['FIRST', '(λ p . p (λ f s . f))'],
-  ['PAIR', '(λ p . p (λ f s . s))'],
-]
 
 // TODO: delete?
 export default {
