38ce27dd3a56f94daaac7653fca766a618c5c917
Taskkill <johny95wil@gmail.com>
2019-05-04 17:41:49 +0200

    export Binary interface, recompile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Token, TokenType, CodeStyle, tokenize } from './lexer'
 export { MacroDef, MacroTable, parse, builtinMacros, MacroMap } from './parser'
-export { AST, Application, Lambda, ChurchNumber, Macro, Variable } from './ast'
+export { AST, Binary, Application, Lambda, ChurchNumber, Macro, Variable } from './ast'
 export { ASTReduction, Alpha, Beta, Expansion, None } from './reductions'
 export { AlphaConvertor, BetaReducer, Expandor, EmptyReducer, constructFor, Reducer } from './reducers'
 
