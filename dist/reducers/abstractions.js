59ca28f56cc67410f74265496bcb08991e4760c8
Taskkill <johny95wil@gmail.com>
2019-10-03 16:27:59 +0200

    implement rest of the macros for simplified strategy

--- dist/reducers/abstractions.js
+++ dist/reducers/abstractions.js
@@ -35,11 +35,78 @@ Abstractions.knownAbstractions = {
             return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
         }
     ],
-    // 'PRED' : [ 1, () => true, () => {} ],
-    // 'SUC' : [ 1, () => true, () => {} ],
-    // 'AND' : [ 2, () => true, () => {} ],
-    // 'OR' : [ 2, () => true, () => {} ],
-    // 'NOT' : [ 1, () => true, () => {} ],
+    'PRED': [
+        1,
+        (args) => {
+            const [first] = args;
+            return args.length === 1 && first instanceof ast_1.ChurchNumeral;
+        },
+        (args) => {
+            const [first] = args;
+            const value = Math.max(0, Number(first) - 1);
+            const lambdaValue = `${value}`;
+            return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
+        }
+    ],
+    'SUC': [
+        1,
+        (args) => {
+            const [first] = args;
+            return args.length === 1 && first instanceof ast_1.ChurchNumeral;
+        },
+        (args) => {
+            const [first] = args;
+            const value = Number(first) + 1;
+            const lambdaValue = `${value}`;
+            return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
+        }
+    ],
+    'AND': [
+        2,
+        (args) => {
+            const [first, second] = args;
+            return args.length === 2 && first instanceof ast_1.Macro && second instanceof ast_1.Macro && (first.name() === 'T' || first.name() === 'F') && (second.name() === 'T' || second.name() === 'F');
+        },
+        (args) => {
+            const [first, second] = args;
+            const firstBoolean = first.name() === 'T' ? true : false;
+            const secondBoolean = second.name() === 'T' ? true : false;
+            const value = firstBoolean && secondBoolean;
+            const lambdaValue = value ? 'T' : 'F';
+            return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
+        }
+    ],
+    'OR': [
+        2,
+        (args) => {
+            const [first, second] = args;
+            return args.length === 2 && first instanceof ast_1.Macro && second instanceof ast_1.Macro && (first.name() === 'T' || first.name() === 'F') && (second.name() === 'T' || second.name() === 'F');
+        },
+        (args) => {
+            const [first, second] = args;
+            const firstBoolean = first.name() === 'T' ? true : false;
+            const secondBoolean = second.name() === 'T' ? true : false;
+            const value = firstBoolean || secondBoolean;
+            const lambdaValue = value ? 'T' : 'F';
+            return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
+        }
+    ],
+    'NOT': [
+        1,
+        (args) => {
+            const [first] = args;
+            return args.length === 1 && first instanceof ast_1.Macro && (first.name() === 'F' || first.name() === 'T');
+        },
+        (args) => {
+            const [first] = args;
+            const name = first.name();
+            const value = name === 'T' ? true : false;
+            const negative = !value;
+            const lambdaValue = negative ? 'T' : 'F';
+            // TODO: if I create Boolean offspring of Macro - this place is to get more simple
+            return parser_1.parse(lexer_1.tokenize(lambdaValue, { lambdaLetters: ['\\'], singleLetterVars: false }), {});
+        }
+    ],
     '+': [
         2,
         (args) => {
