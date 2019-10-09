f6d426c94b85f7851df5a3279a463a6912a6725e
Taskkill <johny95wil@gmail.com>
2019-10-09 20:52:25 +0200

    fix - export Eta conversion from index

--- src/reductions/index.ts
+++ src/reductions/index.ts
@@ -3,6 +3,7 @@ export interface ASTReduction {}
 export { Alpha } from './alpha'
 export { Beta } from './beta'
 export { Expansion } from './expansion'
+export { Eta } from './eta'
 export { None } from './none'
 
 export { Gama, arity } from './gama'
