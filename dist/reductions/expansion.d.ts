746e7559f6198163917c18df20bf8128ea5f5261
jalivert <184747279+jalivert@users.noreply.github.com>
2020-05-23 12:36:43 +0200

    make macroTable optional in Plain type

--- dist/reductions/expansion.d.ts
+++
@@ -1,8 +0,0 @@
-import { Binary, Child, AST } from "../ast";
-import { ASTReduction } from ".";
-export declare class Expansion implements ASTReduction {
-    readonly parent: Binary | null;
-    readonly treeSide: Child | null;
-    readonly target: AST;
-    constructor(parent: Binary | null, treeSide: Child | null, target: AST);
-}
