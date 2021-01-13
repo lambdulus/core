b77afda89c12eaba7fec7aab5412c168ca47a759
jalivert <184747279+jalivert@users.noreply.github.com>
2021-01-13 22:39:46 +0100

    change definitions of builtin macros

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -20,16 +20,18 @@ export interface MacroMap {
 export const builtinMacros : MacroMap = {
   // TODO: uncomment these once PPA students reach them
   'Y' : '(λ f . (λ x . f (x x)) (λ x . f (x x)))',
-  'Z' : '(λ f . (λ y . f (λ z . y y z)) (λ y . f (λ z . y y z)))',
+  // 'Z' : '(λ f . (λ y . f (λ z . y y z)) (λ y . f (λ z . y y z)))',
   'ZERO' : '(λ n . n (λ x . (λ t f . f)) (λ t f . t))',
   // TODO: uncomment these once PPA students reach them
   'PRED' : '(λ x s z . x (λ f g . g (f s)) (λ g . z) (λ u . u))',
   'SUC' : '(λ n s z . s (n s z))',
   'AND' : '(λ x y . x y x)',
-  'OR' : '(λ x y . x x y)',
+  'OR' : '(λ x y . x T y)',
+  'OR2' : '(λ x y . x x y)',
   'T' : '(λ t f . t)',
   'F' : '(λ t f . f)',
-  'NOT' : '(λ p . p F T)',
+  'NOT' : '(λ x t f . x f t)',
+  'NOT2' : '(λ p . p F T)',
   '+' : '(λ x y s z . x s (y s z))',
   '-' : '(λ m n . (n PRED) m)',
   '*' : '(λ x y s . x (y s))',
@@ -42,14 +44,14 @@ export const builtinMacros : MacroMap = {
   '>=' : '(λ m n . ZERO (- n m))',
   '<=' : '(λ m n . ZERO (- m n))',
   // TODO: uncomment these once PPA students reach them
-  'IF' : '(λ p t e . p t e)',
-  'PAIR' : '(λ f s . (λ g . g f s))',
-  'FIRST' : '(λ p . p (λ f s . f))',
-  'SECOND' : '(λ p . p (λ f s . s))',
+  // 'IF' : '(λ p t e . p t e)',
+  // 'PAIR' : '(λ f s . (λ g . g f s))',
+  // 'FIRST' : '(λ p . p (λ f s . f))',
+  // 'SECOND' : '(λ p . p (λ f s . s))',
   
-  'CONS' : '(λ car cdr . (λ g . g car cdr))',
-  'NIL' : '(λx. T)',
-  'NULL' : '(λp.p (λx y.F))',
+  // 'CONS' : '(λ car cdr . (λ g . g car cdr))',
+  // 'NIL' : '(λx. T)',
+  // 'NULL' : '(λp.p (λx y.F))',
 }
 
 // TODO: remove
