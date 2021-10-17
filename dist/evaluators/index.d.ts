7e754adef0b5ac7ea293b9fd33643a7b472548fc
jalivert <184747279+jalivert@users.noreply.github.com>
2021-10-17 14:32:33 +0200

    stop including dist in the github repo

--- dist/evaluators/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { NormalEvaluator } from './normalevaluator';
-import { ApplicativeEvaluator } from './applicativeevaluator';
-import { OptimizeEvaluator } from './optimizeevaluator';
-import { NormalAbstractionEvaluator } from './normalabstractionevaluator';
-export { NormalEvaluator } from './normalevaluator';
-export { ApplicativeEvaluator } from './applicativeevaluator';
-export { OptimizeEvaluator } from './optimizeevaluator';
-export { NormalAbstractionEvaluator } from './normalabstractionevaluator';
-export declare type Evaluator = NormalEvaluator | ApplicativeEvaluator | OptimizeEvaluator | NormalAbstractionEvaluator;
--- dist/reducers/reducerfactory.d.ts
+++
@@ -1,4 +0,0 @@
-import { AST } from "../ast";
-import { Reducer } from './';
-import { ASTReduction } from "../reductions";
-export declare function constructFor(tree: AST, nextReduction: ASTReduction): Reducer;
